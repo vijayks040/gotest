c09f0359ab8318c2c3adf1a5de5a943192703f15
LAPTOP-GFB0PPKF\vijay <vijayk@searchblox.com>
2018-09-01 19:54:36 +0530

    vijay:adding goibibo test code

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+﻿FROM golang:1.10
+RUN mkdir -p /opt/gotest/src/gosam
+RUN mkdir -p /opt/gotest/bin
+RUN mkdir -p /opt/gotest/pkg
+COPY ./ /opt/gotest/src/gosam
+WORKDIR /opt/gotest/src/gosam
+RUN export GOPATH=/opt/gotest
+RUN go get github.com/gocarina/gocsv
+RUN CGO_ENABLED=0 go build -v -o "dist"
+FROM alpine:3.6
+COPY --from=0 /opt/gotest/src/gosam/dist /usr/bin/gosam
+RUN mkdir -p /opt/csvFile
+COPY --from=0 /opt/gotest/src/gosam/csvFile /opt/csvFile
+WORKDIR /opt
+CMD ["gosam", "--help"]
---
+++ goibibo.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gocarina/gocsv"
+)
+
+/*struct to unmarshall the csv values*/
+type CsvLine_csv struct {
+	Key   string `csv:"key,omitempty"`
+	Value string `csv:"value,omitempty"`
+}
+
+func main() {
+	csvFile, err := os.OpenFile("csvFile/Corpus.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	var clients []*CsvLine_csv
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		r := csv.NewReader(in)
+		r.Comma = ','
+		return r
+	})
+	if err := gocsv.UnmarshalFile(csvFile, &clients); err != nil { // Load clients from file
+		fmt.Println("error", err)
+		return
+	}
+	goibibo_map := make(map[string]string)
+	for _, client := range clients {
+		goibibo_map[client.Value] = client.Key
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.EqualFold(goibibo_map[strings.TrimPrefix(r.URL.Path, "/")], "") {
+			fmt.Println("search not found", strings.TrimPrefix(r.URL.Path, "/"))
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			fmt.Println("search found", strings.TrimPrefix(r.URL.Path, "/"))
+			var response_csv CsvLine_csv
+			response_csv.Key = goibibo_map[strings.TrimPrefix(r.URL.Path, "/")]
+			response_csv.Value = strings.TrimPrefix(r.URL.Path, "/")
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			response, _ := json.Marshal(response_csv)
+			w.Write(response)
+		}
+	})
+	fmt.Println("web server listening @ port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
